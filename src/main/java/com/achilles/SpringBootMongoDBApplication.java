ae31b60e6fecd6045eac2e3d3097533d3fd1e2e4
Achilles <532398999@qq.com>
2024-01-16 01:25:46 +0800

    提出redis 和部分mongo log

--- src/main/java/com/achilles/SpringBootMongoDBApplication.java
+++ src/main/java/com/achilles/SpringBootMongoDBApplication.java
@@ -3,6 +3,12 @@ package com.achilles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @SpringBootApplication
@@ -11,9 +17,23 @@ public class SpringBootMongoDBApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringBootMongoDBApplication.class, args);
 
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SpringBootMongoDB SUCCESS ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootMongoDBApplication.class, args);
+		DataSource dataSource = applicationContext.getBean(DataSource.class);
+
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ datasource is : " + dataSource+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+		try {
+			Connection connection = dataSource.getConnection();
+			ResultSet rs = connection.createStatement().executeQuery("SELECT 1");
+			if (rs.first()) {
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Connect database SUCCESS ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			} else {
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Connect database FAIL ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
