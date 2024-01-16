7f0c785edac74c74802ffb1cab5dfb463a7b61fb
Achilles <532398999@qq.com>
2024-01-16 11:37:37 +0800

    rename

--- src/main/java/com/achilles/SpringBootMongoDbMysqlApplication.java
+++ src/main/java/com/achilles/SpringBootMongoDbMysqlApplication.java
@@ -7,18 +7,17 @@ import org.springframework.context.ApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
 @SpringBootApplication
 @ServletComponentScan
-public class SpringBootMongoDBApplication {
+public class SpringBootMongoDbMysqlApplication {
 
 	public static void main(String[] args) {
 
 
-		ApplicationContext applicationContext = SpringApplication.run(SpringBootMongoDBApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootMongoDbMysqlApplication.class, args);
 		DataSource dataSource = applicationContext.getBean(DataSource.class);
 
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ datasource is : " + dataSource+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -1,14 +1,12 @@
 package com.test;
 
-import com.achilles.SpringBootMongoDBApplication;
-import com.achilles.server.controller.SpringBootMongoDBController;
+import com.achilles.SpringBootMongoDbMysqlApplication;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= SpringBootMongoDBApplication.class)
+@SpringBootTest(classes= SpringBootMongoDbMysqlApplication.class)
 public class BaseTest {
 
 }
