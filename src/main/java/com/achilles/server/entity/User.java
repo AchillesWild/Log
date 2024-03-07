b8343d54fe52ccb44e826331d9b552e75082bcf9
Achilles <532398999@qq.com>
2024-03-07 18:35:15 +0800

    register login  ok

--- src/main/java/com/achilles/server/entity/User.java
+++ src/main/java/com/achilles/server/entity/User.java
@@ -1,10 +1,14 @@
 package com.achilles.server.entity;
 
 import com.achilles.model.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class User extends BaseEntity {
 
-    private String uuid;
+    private String userName;
 
     private String nickName;
 
@@ -21,76 +25,4 @@ public class User extends BaseEntity {
     private Integer loginTimes;
 
     private Integer proved;
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public Integer getLoginTimes() {
-        return loginTimes;
-    }
-
-    public void setLoginTimes(Integer loginTimes) {
-        this.loginTimes = loginTimes;
-    }
-
-    public Integer getProved() {
-        return proved;
-    }
-
-    public void setProved(Integer proved) {
-        this.proved = proved;
-    }
 }
